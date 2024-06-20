fbec9224f44d78462c5899d3f40853534b9b520e
Tyler Urban <turban@knackworks.com>
2024-06-20 10:25:21 -0400

    Add goto definition keybinding

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,4 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
   dependencies = { "folke/neodev.nvim" },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      callback = function(event)
+        local bufmap = function(mode, lhs, rhs, desc)
+          local opts = {buffer = event.buf, desc = desc}
+          vim.keymap.set(mode, lhs, rhs, opts)
+        end
+
+        bufmap("n", "gd", vim.lsp.buf.definition, "Jumps to the definition of the symbol under the cursor.")
+      end,
+    })
+  end,
 }
